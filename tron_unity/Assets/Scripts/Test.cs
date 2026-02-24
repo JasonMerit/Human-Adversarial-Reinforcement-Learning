0837481bb5acff075469930f79b024d09ce07ab7
JasonMerit <jasonaronmerit@email.com>
2026-02-24 19:24:03 +0100

    Add kek for comparing across python and unity of Voronoi score

--- tron_unity/Assets/Scripts/Test.cs
+++ tron_unity/Assets/Scripts/Test.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class Test : MonoBehaviour
 {
@@ -22,6 +23,9 @@ public class Test : MonoBehaviour
 
     [HideInInspector] public Color playerColor;
     [HideInInspector] public Color adversaryColor;
+
+    List<Vector2Int> actionHistory = new();
+    List<float> scoreHistory = new();
 
     void Start()
     {
@@ -61,50 +65,50 @@ public class Test : MonoBehaviour
         else if (Keyboard.current.rightArrowKey.wasPressedThisFrame) newAction.y = 1;
         else if (Keyboard.current.downArrowKey.wasPressedThisFrame) newAction.y = 2;
         else if (Keyboard.current.leftArrowKey.wasPressedThisFrame) newAction.y = 3;
+
+        // print histories upon "P" key press
+        if (Keyboard.current.pKey.wasPressedThisFrame) {
+            string log = "[";
+            for (int i = 0; i < actionHistory.Count; i++) { log += $"({actionHistory[i].x}, {actionHistory[i].y}), "; }
+            log += "]\n[";
+            for (int i = 0; i < scoreHistory.Count; i++) { log += $"{scoreHistory[i]}, "; }
+            log += "]";
+            Debug.Log(log);
+        }
         
         if (newAction.x > -1 && !tron.bike1.IsHitInDir(tron.trails, DIRS[newAction.x])) action.x = newAction.x;
         if (newAction.y > -1 && !tron.bike2.IsHitInDir(tron.trails, DIRS[newAction.y])) action.y = newAction.y;
 
         if (action.x != -1 && action.y != -1) { 
             Step();
+            int score = Heuristic.Voronoi(tron.trails, tron.bike1.pos, tron.bike2.pos);
             int[,] ownership = Heuristic.FillBoard(tron.trails, tron.bike1.pos, tron.bike2.pos);
             int playerScore = 0;
             int adversaryScore = 0;
             boardGhost.Clear();
-            for (int x = 0; x < ownership.GetLength(0); x++)
-            {
-                for (int y = 0; y < ownership.GetLength(1); y++)
-                {
-                    if (tron.trails[x, y] != 0)
-                        continue; // skip trails
+            for (int x = 0; x < ownership.GetLength(0); x++) {
+                for (int y = 0; y < ownership.GetLength(1); y++) {
+                    if (tron.trails[x, y] != 0) continue; // skip trails
 
                     Color color;
-
-                    if (ownership[x, y] == 1)
-                    {
+                    if (ownership[x, y] == 1) {
                         color = playerColor;
                         playerScore++;
                     }
-                    else if (ownership[x, y] == 2)
-                    {
+                    else if (ownership[x, y] == 2) {
                         color = adversaryColor;
                         adversaryScore++;
                     }
-                    else if (ownership[x, y] == 3)
-                    {
-                        color = Color.yellow; // battlefront
-                    }
-                    else
-                    {
-                        color = Color.gray;
-                    }
+                    else if (ownership[x, y] == 3) color = Color.yellow; // battlefront
+                    else color = Color.gray;
 
                     color.a = 0.5f;
                     boardGhost.SetCell(new Vector2Int(x, y), color);
                 }
             }
             // outputText.text = "Player: " + playerScore + "\nAdversary: " + adversaryScore;
-            // outputText.text = "Score: " + score;
+            outputText.text = "Score: " + score;
+            scoreHistory.Add(score);
          }
     }
 
@@ -114,6 +118,8 @@ public class Test : MonoBehaviour
         board.SetCell(tron.bike2.pos, adversaryColor);
 
         GameState state = tron.Step(DIRS[action.x], DIRS[action.y]);
+        // Add to history, etc.
+        actionHistory.Add(action);
         action = new(-1,-1);
 
         player.position = new Vector3(tron.bike1.pos.x, tron.bike1.pos.y, 0);
