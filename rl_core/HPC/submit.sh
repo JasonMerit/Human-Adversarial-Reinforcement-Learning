db1b549a73c7e5a046e18f955c68e1d0fd380b61
JasonMerit <Jasonarom@gmail.com>
2026-04-09 13:46:35 +0200

    Fuck smart naming

--- rl_core/HPC/submit.sh
+++ rl_core/HPC/submit.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-name = "Rainbow"
 ### General options
 ###BSUB -q hpc
 #BSUB -q gpuv100
-#BSUB -J $name
+#BSUB -J Tron
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -R "span[hosts=1]"
@@ -25,7 +24,7 @@ for i in {1..5}
 do
     echo "====== [$(date)] Starting run $i ======"
     # python -m rl_core.train_ppo --exp-name PPO
-    python -m rl_core.rainbow.train --exp-name $name --training_frames 10_000_000
+    python -m rl_core.rainbow.train --exp-name Rainbow --training_frames 10_000_000
     # python -m rl_core.self_train_pool --exp-name Pooling --no-save-model --total_timesteps 10 --num-envs 1
     echo ""
 done
