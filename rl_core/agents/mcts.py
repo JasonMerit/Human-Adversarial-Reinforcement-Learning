344152e61e3390dd36dd2948ba014fd332d113f9
JasonMerit <Jasonarom@gmail.com>
2026-05-07 16:43:05 +0200

    Oops

--- rl_core/agents/mcts.py
+++ rl_core/agents/mcts.py
@@ -18,14 +18,14 @@ if __name__ == "__main__":
     NUM_ENVS=5
     agent = MCTSAgent(TronDuoEnv(SIZE), VecTronDuoEnv(NUM_ENVS,SIZE), rollouts=100)
     env = TronDuoEnv(SIZE)
-    env.reset()
-    state = infos["state"]
+    obs, info = env.reset()
+    state = info["state"]
 
     # Populate buffer with random samples just to have something to train on
     for _ in range(200):
         action = env.action_space.sample()
-        next_obs, reward, done, _, infos = env.step(action)
-        state = infos["state"]
+        next_obs, reward, done, _, info = env.step(action)
+        state = info["state"]
         agent.rb.add(state, action, reward, next_state, done)
         next_state = state
 
